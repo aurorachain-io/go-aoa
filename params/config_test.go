d1c7a0975e1dda0b9f2860017c22c2d7546c539a
aurorachainio <59164220+aurorachainio@users.noreply.github.com>
2024-03-26 14:38:22 +0800

    test tie timestamp based forks

--- params/config_test.go
+++ params/config_test.go
@@ -46,3 +46,22 @@ func TestCheckCompatible(t *testing.T) {
 		}
 	}
 }
+
+func TestConfigRules(t *testing.T) {
+	c := &ChainConfig{
+		LondonBlock:  new(big.Int),
+		ShanghaiTime: newUint64(450),
+	}
+	var stamp uint64
+	if r := c.Rules(big.NewInt(1), true, stamp); r.IsShanghai {
+		t.Errorf("expected %v to not be", stamp)
+	}
+	stamp = 450
+	if r := c.Rules(big.NewInt(1), true, stamp); !r.IsShanghai {
+		t.Errorf("expected %v to be", stamp)
+	}
+	stamp = math.MaxInt64
+	if r := c.Rules(big.NewInt(1), true, stamp); !r.IsShanghai {
+		t.Errorf("expected %v to be", stamp)
+	}
+}
